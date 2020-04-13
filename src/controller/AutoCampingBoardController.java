1f0d7731d49f43b2d1bc468d13ab0de96bfdbfce
Hong <Hong@DESKTOP-MSRCCVD>
2020-04-13 14:55:57 +0900

    imgUpload 메소드 수정

--- src/controller/AutoCampingBoardController.java
+++ src/controller/AutoCampingBoardController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -155,6 +156,7 @@ public class AutoCampingBoardController {
 	
 	
 	@PostMapping(value="/imgUpload")
+	@RequestMapping(value="/imgUpload" ,method = {RequestMethod.GET, RequestMethod.POST})
 	@ResponseBody
 	public Map<String, Object> autoCampingImgUpload(MultipartHttpServletRequest request) throws IOException {
 
